05894951cf2d1f8d721b7a44f40c4b99ee302bf2
giflamiano <gflamiano@gmail.com>
2024-11-24 16:32:57 -0500

    adding search input for rides, dining, and shops

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,5 +1,5 @@
 function openModal(name, description, type, heightReq, location, imgPath) {
-    const modal = document.getElementById('rideModal');
+    const modal = document.getElementById('rideModal')
     const modalImg = document.getElementById("modalImg");
     const modalTitle = document.getElementById("modalTitle");
     const modalDesc = document.getElementById("modalDesc");
@@ -22,5 +22,54 @@ function openModal(name, description, type, heightReq, location, imgPath) {
         if (event.target == modal) modal.style.display = 'none';
     });
     
+}
 
+function openDiningModal(name, description, type, dietaryOptions, location, imgPath) {
+    const modal = document.getElementById('diningModal')
+    const modalImg = document.getElementById("modalImg");
+    const modalTitle = document.getElementById("modalTitle");
+    const modalDesc = document.getElementById("modalDesc");
+    const modalType = document.getElementById("modalType");
+    const modalHeight = document.getElementById("modalDietaryOptions");
+    const modalLocation = document.getElementById("modalLocation");
+
+    modalImg.src = imgPath;
+    modalTitle.textContent = name;
+    modalDesc.textContent = description;
+    modalType.innerHTML = `<strong>Cuisine:</strong> ${type}`;
+    modalHeight.innerHTML = `<strong>Dietary Options:</strong> ${dietaryOptions}`;
+    modalLocation.innerHTML = `<strong>Located In:</strong> ${location}`;
+
+    modal.style.display = 'flex';
+    modal.style.flexDirection = 'column';
+    modal.style.alignItems = 'center';
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) modal.style.display = 'none';
+    });
+    
+}
+
+function openShopsModal(name, description, type, location, imgPath) {
+    const modal = document.getElementById('diningModal')
+    const modalImg = document.getElementById("modalImg");
+    const modalTitle = document.getElementById("modalTitle");
+    const modalDesc = document.getElementById("modalDesc");
+    const modalType = document.getElementById("modalType");
+    const modalLocation = document.getElementById("modalLocation");
+
+    modalImg.src = imgPath;
+    modalTitle.textContent = name;
+    modalDesc.textContent = description;
+    modalType.innerHTML = `<strong>Type:</strong> ${type}`;
+    modalLocation.innerHTML = `<strong>Located In:</strong> ${location}`;
+
+    modal.style.display = 'flex';
+    modal.style.flexDirection = 'column';
+    modal.style.alignItems = 'center';
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) modal.style.display = 'none';
+    });
+    
 }
