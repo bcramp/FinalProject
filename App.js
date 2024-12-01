5ee0745ec4f50d54485c47c3d867e24dbf723a66
giflamiano <gflamiano@gmail.com>
2024-11-30 20:25:59 -0500

    adding functionality to insert existing or new customers into db when placing an order

--- App.js
+++ App.js
@@ -4,10 +4,13 @@
 //******************************************************************************
 const express = require("express");
 const favicon = require('serve-favicon');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
-app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(cors());
+app.use(express.json());
 
 //*** server waits indefinitely for incoming requests
 app.listen(port, function () {
@@ -44,6 +47,7 @@ con.connect(function(err) {
 //******************************************************************************
 
 const fs = require("fs");
+const exp = require("constants");
 
 function readAndServe(path, res)
 {
@@ -105,6 +109,10 @@ app.get("/shop", function (req, res) {
 
 // CHECKOUT
 app.get("/checkout", function (req, res) {
+  readAndServe("./public/html/checkout-form.html", res)
+});
+
+app.get("/checkout/cart", function (req, res) {
   readAndServe("./public/html/checkout.html", res)
 });
 
@@ -113,6 +121,37 @@ app.get("/account", function (req, res) {
   readAndServe("./public/html/account.html", res)
 });
 
+app.get("/confirmation", function (req, res) {
+  readAndServe('./public/html/confirmation.html', res)
+})
+
+app.post("/account", (req, res) => {
+  const { email, password } = req.body
+  // var query = `SELECT * FROM customer WHERE email='${email}' AND password='${password}'`;
+  var query = `SELECT c.first_name, pl.order_id, pr.name, pr.price, o.purchase_datetime
+FROM customer c
+JOIN place pl ON c.cust_id = pl.cust_id
+JOIN order o ON pl.order_id = o.order_id
+JOIN contain ct ON o.order_id = ct.order_id
+JOIN product pr ON ct.product_id = pr.product_id
+                WHERE c.email = '${email}' AND c.password='${password}';`;
+  con.query(query, [email, password], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    if (results.length > 0 ) {
+      
+      res.json({
+        success: true,
+        data: results
+      });
+    } else {
+        res.json({
+          success: false, 
+          message: 'Invalid email or password.'
+        })
+    }
+  });
+})
 
 
 
@@ -141,7 +180,7 @@ app.get("/getThrillRides", function (req, res) {
   con.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
-      res.starus(500).send('Server Error');
+      res.status(500).send('Server Error');
       return;
     } res.json(results);
   });
@@ -207,46 +246,183 @@ app.get("/getShops", function (req, res) {
   });
 });
 
+// PRODUCTS
+app.get("/getProducts", function (req, res) {
+  var query = `SELECT * FROM product`;
+  con.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Server Error');
+      return;
+    } res.json(results);
+  });
+});
+
+
+// POST ROUTE FOR WHEN CUSTOMER PLACES AN ORDER
+app.post("/order-confirm", (req, res) => {
+  const { customer, products } = req.body;
+  const { first_name, last_name, email, password, card_number, expiry_date, cvv } = customer;
+  var baseTotal = 0;
+
+  // filters out products to only get the ones included in the order
+  for (const key in products) {
+    products[key].forEach(item => {
+      if (item.quantity == 0) delete products[key];
+      else {
+        baseTotal += parseFloat(item.quantity) * parseFloat(item.price.slice(1));
+      }
+    });
+  }
+  
+  var baseTotalRounded = baseTotal.toFixed(2)
+  var tax = baseTotal * 0.06;
+  var taxRounded = tax.toFixed(2);
+  var total = parseFloat(baseTotalRounded) + parseFloat(taxRounded);
+  var totalRounded = total.toFixed(2);
+  baseTotal = parseFloat(baseTotalRounded);
+  total = parseFloat(totalRounded);
+  const datetime = new Date();
+  const mm = datetime.getMonth() + 1;
+  const dd = datetime.getDate();
+  const yyyy = datetime.getFullYear();
+  const purchaseDate =  mm + '/' + dd + '/' + yyyy;
+
+  // // Validate password match
+  // if (password !== confirmPassword) {
+  //     return res.status(400).send("Passwords do not match.");
+  // }
+
+  // Check for existing customer
+  const checkQuery = `SELECT * FROM customer WHERE email = ?`;
+
+  con.query(checkQuery, [email], (err, results) => {
+      if (err) {
+          console.error("Error checking for existing customer:", err);
+          return res.status(500).send("An error occurred while checking the customer.");
+      } else if (results.length > 0) { // existing customer
+        if (password != results[0].password) return res.status(500).json({ message: `You are an existing <Hello World/> customer, but you entered the incorrect password for ${email}. Please try again.` });
+          const cust_id = results[0].cust_id;
+          const insertOrderQuery = `INSERT INTO \`order\` (purchase_date, base_price, total_price) VALUES ('${purchaseDate}', ${baseTotal}, ${total})`;
+          con.query(insertOrderQuery, (err, orderResult) => {
+            if (err) {
+              console.error("Error inserting order:", err);
+              return res.status(500).send("Failed to add order.");
+            }
+
+            const order_id = orderResult.insertId;
+            const insertPlaceQuery = `INSERT INTO place (cust_id, order_id) VALUES (${cust_id}, ${order_id})`;
+            
+            con.query(insertPlaceQuery, (err, placeResult) => {
+              if (err) {
+                console.error("Error inserting place:", err);
+                return res.status(500).send("Failed to add place details.");
+              }
+              for (const key in products) {
+                products[key].forEach(item => {
+                  const expireDate = [2, 3, 7, 8].includes(item.productId) ? '2025-12-31' : null;
+                  const insertContainQuery = `INSERT INTO contain (order_id, product_id, quantity, valid_start_date, expire_date) VALUES (${order_id}, ${item.productId}, ${[item.quantity]}, '${purchaseDate}', '${expireDate}')`;
+                  con.query(insertContainQuery, (err, containResult) => {
+                    if (err) {
+                      console.error("Error insert contain:", err);
+                      return res.status(500).send("Failed to add contain details.");
+                    } 
+                  })
+                });
+              }       
+              res.send({ message: 'Customer and order added successfully!' });       
+            })
+          })    
+      } else { // new customer
+          // Insert the new customer into the customer table
+          const insertCustomerQuery = `INSERT INTO customer (first_name, last_name, email, password) VALUES ('${first_name}', '${last_name}', '${email}', '${password}')`;
+      
+          con.query(
+              insertCustomerQuery, (err, customerResult) => {
+                if (err) {
+                  console.error("Error inserting customer:", err);
+                  return res.status(500).send("Failed to add customer.");
+                } 
+
+                const cust_id = customerResult.insertId;
+                const insertOrderQuery = `INSERT INTO \`order\` (purchase_date, base_price, total_price) VALUES ('${purchaseDate}', ${baseTotal}, ${total})`;
+                con.query(insertOrderQuery, (err, orderResult) => {
+                  if (err) {
+                    console.error("Error inserting order:", err);
+                    return res.status(500).send("Failed to add order.");
+                  }
+
+                  const order_id = orderResult.insertId;
+                  const insertPlaceQuery = `INSERT INTO place (cust_id, order_id) VALUES (${cust_id}, ${order_id})`;
+                  
+                  con.query(insertPlaceQuery, (err, placeResult) => {
+                    if (err) {
+                      console.error("Error inserting place:", err);
+                      res.status(500).send("Failed to add place details.");
+                    }
+                    for (const key in products) {
+                      products[key].forEach(item => {
+                        const expireDate = [2, 3, 7, 8].includes(item.productId) ? '2025-12-31' : null;
+                        const insertContainQuery = `INSERT INTO contain (order_id, product_id, quantity, valid_start_date, expire_date) VALUES (${order_id}, ${item.productId}, ${[item.quantity]}, '${purchaseDate}', '${expireDate}')`;
+                        con.query(insertContainQuery, (err, containResult) => {
+                          if (err) {
+                            console.error("Error insert contain:", err);
+                            return res.status(500).send("Failed to add contain details.");
+                          } 
+                        })
+                      });
+                    }       
+                    res.send({ message: 'Customer and order added successfully!' });       
+                  })
+              })
+                
+              }
+          );
+      }
+  });
+});
+
+
 
 //******************************************************************************
 //*** receive post register data from the client
 //******************************************************************************
-app.post("/search", function (req, res) {
-    var desc = req.body.desc;   // extract the strings received from the browser
+// app.post("/search", function (req, res) {
+//     var desc = req.body.desc;   // extract the strings received from the browser
 
-    var sql_query = "select title, description from film where description like '%" + desc + "%'";
+//     var sql_query = "select title, description from film where description like '%" + desc + "%'";
 
-    con.query(sql_query, function (err, result, fields) { // execute the SQL string
-		if (err)
-		    throw err;                  // SQL error
+//     con.query(sql_query, function (err, result, fields) { // execute the SQL string
+// 		if (err)
+// 		    throw err;                  // SQL error
 
-	    else {
+// 	    else {
 
-                  //*** start creating the html body for the browser
-			      var html_body = "<HTML><STYLE>body{font-family:arial}</STYLE>";
-			      html_body = html_body + "<BODY><TABLE BORDER=1>";
+//                   //*** start creating the html body for the browser
+// 			      var html_body = "<HTML><STYLE>body{font-family:arial}</STYLE>";
+// 			      html_body = html_body + "<BODY><TABLE BORDER=1>";
 
-			      //*** print column headings
-			      html_body = html_body + "<TR>";
-                 for (var i = 0; i < fields.length; i++)
-				    html_body = html_body + ("<TH>" + fields[i].name.toUpperCase() + "</TH>");
-				  html_body = html_body + "</TR>";
+// 			      //*** print column headings
+// 			      html_body = html_body + "<TR>";
+//                  for (var i = 0; i < fields.length; i++)
+// 				    html_body = html_body + ("<TH>" + fields[i].name.toUpperCase() + "</TH>");
+// 				  html_body = html_body + "</TR>";
 
-                  //*** prints rows of table data
-				  for (var i = 0; i < result.length; i++)
-				       html_body = html_body + ("<TR><TD>" + result[i].title + "</TD>" + "<TD>" + result[i].description + "</TD></TR>");
+//                   //*** prints rows of table data
+// 				  for (var i = 0; i < result.length; i++)
+// 				       html_body = html_body + ("<TR><TD>" + result[i].title + "</TD>" + "<TD>" + result[i].description + "</TD></TR>");
 
-                  html_body = html_body + "</TABLE>";
+//                   html_body = html_body + "</TABLE>";
 
-				  //** finish off the html body with a link back to the search page
-				  html_body = html_body + "<BR><BR><BR><a href=http://localhost:3000/search>Go Back To Search</a><BR><BR><BR>";
-			      html_body = html_body + "</BODY></HTML>";
+// 				  //** finish off the html body with a link back to the search page
+// 				  html_body = html_body + "<BR><BR><BR><a href=http://localhost:3000/search>Go Back To Search</a><BR><BR><BR>";
+// 			      html_body = html_body + "</BODY></HTML>";
 
-                console.log(html_body);             // send query results to the console
-			    res.send(html_body);                // send query results back to the browser
-	         }
-    });
-});
+//                 console.log(html_body);             // send query results to the console
+// 			    res.send(html_body);                // send query results back to the browser
+// 	         }
+//     });
+// });
 
 
 
