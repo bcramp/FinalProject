f2ad6c8b14816df21412791b04cabedb5a1c4c12
bcramp <77287799+bcramp@users.noreply.github.com>
2024-12-01 18:49:46 -0500

    chore: fixes statuses and searches

--- App.js
+++ App.js
@@ -18,17 +18,30 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+
 //******************************************************************************
 //*** set up mysql connections
 // ******************************************************************************
 var mysql = require('mysql');
 
+// Local connection
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",  // use your own MySQL root password
+//   database: ""
+// });
+
+// Server connection
+// Reminder: Connect to google cloud instance in MySql first then it will work
 var con = mysql.createConnection({
-    host: "localhost",
+    host: "34.133.112.104",
     user: "root",
-    password: "",  // use your own MySQL root password
-    database: ""
+    password: "csc621pass",  // use your own MySQL root password
+    database: "hello_world"
 });
+
 
 //*** connect to the database
 con.connect(function(err) {
@@ -36,6 +49,8 @@ con.connect(function(err) {
         throw err;
     console.log("Connected to MySQL");
 });
+
+
 
 
 //******************************************************************************
@@ -53,6 +68,8 @@ function readAndServe(path, res)
 }
 
 
+
+
 //******************************************************************************
 //*** ROUTES
 //******************************************************************************
@@ -60,6 +77,7 @@ function readAndServe(path, res)
 app.get("/", function (req, res) {
     readAndServe("./public/html/index.html", res)
 });
+
 
 // TICKETS & PASSES PAGES
 app.get("/tickets", function (req, res) {
@@ -73,6 +91,7 @@ app.get("/day-add-ons", function (req, res) {
 app.get("/pass-add-ons", function (req, res) {
     readAndServe("./public/html/pass-add-ons.html", res)
 });
+
 
 // RIDES PAGES
 app.get("/rides", function (req, res) {
@@ -101,6 +120,7 @@ app.get("/shop", function (req, res) {
   readAndServe("./public/html/shops.html", res)
 });
 // --- end routes --- 
+
 
 // CHECKOUT
 app.get("/checkout", function (req, res) {
@@ -140,11 +160,10 @@ app.get("/getThrillRides", function (req, res) {
   con.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
-      res.starus(500).send('Server Error');
+      res.status(500).send('Server Error');
       return;
     } res.json(results);
-  });
-              
+  });           
 })
 
 app.get("/getFamilyRides", function (req, res) {
@@ -155,7 +174,7 @@ app.get("/getFamilyRides", function (req, res) {
   con.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
-      res.starus(500).send('Server Error');
+      res.status(500).send('Server Error');
       return;
     } res.json(results);
   });
@@ -170,7 +189,7 @@ app.get("/getKidsRides", function (req, res) {
   con.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
-      res.starus(500).send('Server Error');
+      res.status(500).send('Server Error');
       return;
     } res.json(results);
   });
@@ -207,42 +226,61 @@ app.get("/getShops", function (req, res) {
 });
 
 
+
+
 //******************************************************************************
 //*** receive post register data from the client
 //******************************************************************************
 app.post("/search", function (req, res) {
     var desc = req.body.desc;   // extract the strings received from the browser
 
-    var sql_query = "select title, description from film where description like '%" + desc + "%'";
+    // var sql_query = "select title, description from film where description like '%" + desc + "%'";
+
+    var sql_query = "select title, description from ride where description like '%" + desc + "%'";
+
+      /*
+    Searching for specific rides
+
+    SELECT *
+    FROM ride
+    WHERE name IS LIKE '%${searchQuery}%' OR type LIKE '%${searchQuery}%' OR description LIKE '%${searchQuery}%';
+    */
+
+    /*
+    Searching for specific types of rides via the clicked link
+
+    SELECT *
+    FROM ride
+    WHERE type = '%${searchQuery}%';
+    */
 
     con.query(sql_query, function (err, result, fields) { // execute the SQL string
 		if (err)
 		    throw err;                  // SQL error
+    else {
+          //*** start creating the html body for the browser
+          var html_body = "<HTML><STYLE>body{font-family:arial}</STYLE>";
+          html_body = html_body + "<BODY><TABLE BORDER=1>";
 
-	    else {
-            //*** start creating the html body for the browser
-            var html_body = "<HTML><STYLE>body{font-family:arial}</STYLE>";
-            html_body = html_body + "<BODY><TABLE BORDER=1>";
+          //*** print column headings
+          html_body = html_body + "<TR>";
+          for (var i = 0; i < fields.length; i++)
+          html_body = html_body + ("<TH>" + fields[i].name.toUpperCase() + "</TH>");
+          html_body = html_body + "</TR>";
 
-            //*** print column headings
-            html_body = html_body + "<TR>";
-            for (var i = 0; i < fields.length; i++)
-            html_body = html_body + ("<TH>" + fields[i].name.toUpperCase() + "</TH>");
-            html_body = html_body + "</TR>";
+          //*** prints rows of table data
+          for (var i = 0; i < result.length; i++)
+              html_body = html_body + ("<TR><TD>" + result[i].title + "</TD>" + "<TD>" + result[i].description + "</TD></TR>");
 
-            //*** prints rows of table data
-            for (var i = 0; i < result.length; i++)
-                html_body = html_body + ("<TR><TD>" + result[i].title + "</TD>" + "<TD>" + result[i].description + "</TD></TR>");
+          html_body = html_body + "</TABLE>";
 
-            html_body = html_body + "</TABLE>";
+          //** finish off the html body with a link back to the search page
+          html_body = html_body + "<BR><BR><BR><a href=http://localhost:3000/search>Go Back To Search</a><BR><BR><BR>";
+          html_body = html_body + "</BODY></HTML>";
 
-            //** finish off the html body with a link back to the search page
-            html_body = html_body + "<BR><BR><BR><a href=http://localhost:3000/search>Go Back To Search</a><BR><BR><BR>";
-            html_body = html_body + "</BODY></HTML>";
-
-            console.log(html_body);             // send query results to the console
-            res.send(html_body);                // send query results back to the browser
-        }
+          console.log(html_body);             // send query results to the console
+          res.send(html_body);                // send query results back to the browser
+      }
     });
 });
 
